e2967a7a19e880743612c779d65dbf9906d8be5d
himanushi <himanushi@users.noreply.github.com>
2024-09-15 19:35:25 +0900

    .gitignoreの追加とCORS設定の実装

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use(
+  '/*',
+  cors({
+    origin: ['https://example.com', 'https://example.org'],
+  })
+)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
