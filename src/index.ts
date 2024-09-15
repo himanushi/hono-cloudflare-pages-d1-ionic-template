0065002ce68081721a495522a81ab8afddc3677f
himanushi <himanushi@users.noreply.github.com>
2024-09-15 19:47:06 +0900

    コードの整形とVSCode設定ファイルの追加

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
 app.use(
-  '/*',
-  cors({
-    origin: ['https://example.com', 'https://example.org'],
-  })
-)
+	"/*",
+	cors({
+		origin: ["https://example.com", "https://example.org"],
+	}),
+);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
