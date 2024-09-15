d0a2b453c44f5396e23142fc252f963a0d6c5c82
himanushi <himanushi@users.noreply.github.com>
2024-09-15 20:13:58 +0900

    biome設定ファイルの追加

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { Hono } from "hono";
+import { compress } from "hono/compress";
 import { cors } from "hono/cors";
 
 const app = new Hono();
 
+app.use(compress());
 app.use(
-	"/*",
-	cors({
-		origin: ["https://example.com", "https://example.org"],
-	}),
+  "/*",
+  cors({
+    origin: ["https://example.com", "https://example.org"],
+  }),
 );
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+  return c.text("Hello Hono!");
 });
 
 export default app;
