db5a0c5193e278cd6e264d18ed91cf54e6617260
himanushi <himanushi@users.noreply.github.com>
2024-09-16 18:26:25 +0900

    スタイルシートの修正とコードの整形

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,17 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
+import { useState } from "hono/jsx";
+import { jsxRenderer } from "hono/jsx-renderer";
 
 export const renderer = jsxRenderer(({ children }) => {
+  const [count, setCount] = useState(0);
   return (
     <html>
       <head>
         <link href="/static/style.css" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <button onClick={() => setCount((p) => p + 1)}>count{count}</button>
+      </body>
     </html>
-  )
-})
+  );
+});
