b4bab824914b2a0e990c5e9cd41ca3d6ed85cc70
himanushi <himanushi@users.noreply.github.com>
2024-09-19 21:05:27 +0900

    @hono/zod-validatorの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import ReactDOMServer from "react-dom/server";
+import { z } from "zod";
 
 const app = new Hono();
 
-app.get("/api/clock", (c) => {
-  return c.json({
-    time: new Date().toLocaleTimeString(),
-  });
-});
+const route = app.get(
+  "/hello",
+  zValidator(
+    "query",
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  (c) => {
+    const { name } = c.req.valid("query");
+    return c.json({
+      message: `Hello! ${name}`,
+    });
+  },
+);
+
+export type AppType = typeof route;
 
 app.get("*", (c) => {
   return c.html(
     ReactDOMServer.renderToString(
-      <html lang="ja">
+      <html lang="en">
         <head>
           {import.meta.env.PROD ? (
             <script type="module" src="/static/client.js" />
