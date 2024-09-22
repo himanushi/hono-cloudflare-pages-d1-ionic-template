87d9f297763313afe33eb8bfc8bf56b091207309
himanushi <himanushi@users.noreply.github.com>
2024-09-22 22:05:44 +0900

    レイアウトコンポーネントの追加とCORSミドルウェアの導入

--- src/serverRoutes.tsx
+++ src/serverRoutes.tsx
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { meApi } from "~/features/me/server/meApi";
 import { usersApi } from "~/features/users/server/usersApi";
 import { serverApi } from "./server";
 
 const app = new Hono();
+
+app.use("*", cors());
 
 const _usersApi = app.get("/api/users", ...usersApi);
 export type UsersAPI = typeof _usersApi;
