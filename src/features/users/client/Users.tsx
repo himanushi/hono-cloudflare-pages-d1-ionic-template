7d8c98b96635f3c78d55847978102c90172a6c2b
himanushi <himanushi@users.noreply.github.com>
2024-09-26 09:10:52 +0900

    ユーザー取得のAPI呼び出しを修正

--- src/features/users/client/Users.tsx
+++ src/features/users/client/Users.tsx
@@ -1,5 +1,10 @@
 import { Box, Button, Flex } from "@yamada-ui/react";
-import { hc } from "hono/client";
+import {
+  type ClientRequestOptions,
+  type ClientResponse,
+  hc,
+} from "hono/client";
+import { useCallback } from "react";
 import useSWRInfinite from "swr/infinite";
 import { useLazyFetch } from "~/hooks/useLazyFetch";
 import type { UsersAPI } from "~/serverRoutes";
@@ -8,22 +13,27 @@ import { fetcher } from "~/utils/fetcher";
 
 const client = hc<UsersAPI>(clientUrl);
 
+const limit = 10;
+
 export const Users = () => {
-  const limit = 10;
-  const { data, setSize } = useSWRInfinite(
-    (pageIndex, _previousPageData) => [client.api.users.$get, pageIndex],
-    async (props) => {
-      console.log(props);
-      const api = props[0];
-      const pageIndex = props[1];
-      return await fetcher(api)({
-        query: {
-          limit: limit.toString(),
-          offset: (pageIndex * limit).toString(),
-        },
-      })();
-    },
+  const getKey = useCallback(
+    (pageIndex: number) => `/api/users?limit=10&offset=${pageIndex * limit}`,
+    [],
   );
+
+  const infiniteFetcher = useCallback(async (key: string) => {
+    const url = new URL(key, clientUrl);
+    const limit = url.searchParams.get("limit") || "10";
+    const offset = url.searchParams.get("offset") || "0";
+
+    const response = await client.api.users.$get({
+      query: { limit, offset },
+    });
+
+    return await response.json();
+  }, []);
+
+  const { data, setSize } = useSWRInfinite(getKey, infiniteFetcher);
 
   const users = (data ?? []).flat();
 
