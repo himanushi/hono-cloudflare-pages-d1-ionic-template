bd7823ed0d01004f5a6fa5e6dcbfd97a992e1a92
himanushi <himanushi@users.noreply.github.com>
2024-09-29 22:43:58 +0900

    useQueryへの移行

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import useSWR, { type SWRConfiguration } from "swr";
 import type { ApiFunction } from "~/types/ApiFunction";
 import { fetcher } from "~/utils/fetcher";
@@ -7,15 +8,35 @@ export const useFetch = <ARGS, RESPONSE>({
   api,
   args,
   skip = false,
-  ...options
 }: {
   key?: string;
   api: ApiFunction<ARGS, RESPONSE> | null;
   args: ARGS;
   skip?: boolean;
-} & SWRConfiguration) =>
-  useSWR(
-    skip ? undefined : [key, args],
-    api ? fetcher(api)(args as NonNullable<ARGS>) : null,
-    options,
-  );
+}) =>
+  useQuery({
+    queryKey: [key],
+    queryFn: () => {
+      if (!skip && api) {
+        return fetcher(api)(args as NonNullable<ARGS>)();
+      }
+    },
+  });
+
+// export const useFetch = <ARGS, RESPONSE>({
+//   key = "api",
+//   api,
+//   args,
+//   skip = false,
+//   ...options
+// }: {
+//   key?: string;
+//   api: ApiFunction<ARGS, RESPONSE> | null;
+//   args: ARGS;
+//   skip?: boolean;
+// } & SWRConfiguration) =>
+//   useSWR(
+//     skip ? undefined : [key, args],
+//     api ? fetcher(api)(args as NonNullable<ARGS>) : null,
+//     options,
+//   );
