8961591a95a953a5536ad95e9de02288e910a92e
himanushi <himanushi@users.noreply.github.com>
2024-09-30 16:44:32 +0900

    ユーザー一覧取得方法の改善

--- src/features/users/client/Users.tsx
+++ src/features/users/client/Users.tsx
@@ -1,7 +1,6 @@
+import { useInfiniteQuery, useMutation } from "@tanstack/react-query";
 import { Box, Button, Flex } from "@yamada-ui/react";
 import { hc } from "hono/client";
-import { useInfiniteFetch } from "~/hooks/useInfiniteFetch";
-import { useLazyFetch } from "~/hooks/useLazyFetch";
 import type { UsersAPI } from "~/serverRoutes";
 import { clientUrl } from "~/utils/clientUrl";
 
@@ -9,69 +8,61 @@ const client = hc<UsersAPI>(clientUrl);
 const limit = 10;
 
 export const Users = () => {
-  const {
-    items: users,
-    hasNext,
-    setSize,
-    ...response
-  } = useInfiniteFetch({
-    getKey: (pageIndex) =>
-      `/api/users?limit=${limit}&offset=${pageIndex * limit}`,
-    fetcher: async (key: string) => {
-      const url = new URL(key, clientUrl);
-      const limit = url.searchParams.get("limit");
-      const offset = url.searchParams.get("offset");
-      if (limit !== null && offset !== null) {
-        const response = await client.api.users.$get({
-          query: { limit, offset },
-        });
-        return await response.json();
-      }
-      return [];
+  const { data, fetchNextPage } = useInfiniteQuery({
+    queryKey: ["users"],
+    queryFn: async ({ pageParam }) => {
+      const res = await client.api.users.$get({
+        query: { limit: limit.toString(), offset: pageParam.offset.toString() },
+      });
+
+      return await res.json();
+    },
+    initialPageParam: { offset: 0, limit },
+    getNextPageParam: (lastPage, _allPages, lastPageParam, _allPageParams) =>
+      lastPage.length === 0
+        ? undefined
+        : { offset: lastPageParam.offset + limit, limit },
+  });
+
+  useMutation({
+    mutationKey: ["createUser"],
+    mutationFn: async () => {
+      const res = await client.api.users.$post({
+        json: { name: "test" },
+      });
+
+      return await res.json();
     },
   });
 
-  const [create] = useLazyFetch({
-    key: "postUsers",
-    api: client.api.users.$post,
-    args: { json: { name: "test" } },
-  });
+  const users = (data ?? { pages: [] }).pages.flat();
 
   return (
     <Flex flexDirection="column">
-      <Button
-        onClick={() => {
-          console.log(response);
-        }}
-      >
-        ログイン
-      </Button>
       {users.map((user) => (
         <Box key={user.id}>
           {user.name}
           {user.id}
         </Box>
       ))}
-      <Button
+      {/* <Button
         onClick={async () => {
           await create();
         }}
       >
         Users
-      </Button>
-      {hasNext && (
-        <Button
-          onClick={() => {
-            setSize((prev) => prev + 1);
-          }}
-        >
-          Next
-        </Button>
-      )}
+      </Button> */}
       <Button
         onClick={() => {
-          setSize((prev) => prev + 1);
+          fetchNextPage();
         }}
+      >
+        Next
+      </Button>
+      <Button
+      // onClick={() => {
+      //   refetch();
+      // }}
       >
         Reset
       </Button>
