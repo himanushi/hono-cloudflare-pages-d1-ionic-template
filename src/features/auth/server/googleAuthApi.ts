e3f551f66156fac55a02108ff57cef55cc470832
himanushi <himanushi@users.noreply.github.com>
2024-10-01 21:14:55 +0900

    Google認証APIのユーザー登録機能追加

--- src/features/auth/server/googleAuthApi.ts
+++ src/features/auth/server/googleAuthApi.ts
@@ -1,8 +1,39 @@
 import { getAuth, processOAuthCallback, revokeSession } from "@hono/oidc-auth";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
+import { setCookie } from "hono/cookie";
 import { createFactory } from "hono/factory";
+import { users } from "~/schema";
 
 export const googleAuthLoginApi = createFactory().createHandlers(async (c) => {
   const auth = await getAuth(c);
+  const sub = auth?.sub;
+
+  if (typeof sub !== "string") {
+    return c.text("No sub found", 400);
+  }
+
+  const db = drizzle(c.env.DB);
+
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.googleUserId, sub))
+    .all();
+
+  if (existingUser.length === 0) {
+    await db
+      .insert(users)
+      .values({ googleUserId: sub, name: "未設定" })
+      .execute();
+  }
+
+  setCookie(c, "session", sub, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  });
+
   return c.text(`Hello ${auth?.sub}!`);
 });
 
