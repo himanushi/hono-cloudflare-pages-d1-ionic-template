e25805cf0350dff741de1f8e7b6ba14afe41184d
himanushi <himanushi@users.noreply.github.com>
2024-10-02 08:46:37 +0900

    ユーザーテーブルにgoogleUserIdインデックス追加

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,20 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable("users", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  name: text("name"),
-  googleUserId: text("googleUserId").unique(),
-  createdAt: integer("created_at", { mode: "timestamp" }).default(
-    sql`(strftime('%s', 'now'))`,
-  ),
-  updatedAt: integer("updated_at", { mode: "timestamp" }).default(
-    sql`(strftime('%s', 'now'))`,
-  ),
-});
+export const users = sqliteTable(
+  "users",
+  {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: text("name"),
+    googleUserId: text("googleUserId").unique(),
+    createdAt: integer("created_at", { mode: "timestamp" }).default(
+      sql`(strftime('%s', 'now'))`,
+    ),
+    updatedAt: integer("updated_at", { mode: "timestamp" }).default(
+      sql`(strftime('%s', 'now'))`,
+    ),
+  },
+  (table) => ({
+    googleUserIdIndex: index("idx_googleUserId").on(table.googleUserId),
+  }),
+);
