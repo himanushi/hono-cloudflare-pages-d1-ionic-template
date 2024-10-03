c92c9fd0b95694a26d660b89c47018fb1251b86b
himanushi <himanushi@users.noreply.github.com>
2024-10-03 20:30:31 +0900

    名前入力機能の追加と保存処理の実装

--- src/features/me/client/Me.tsx
+++ src/features/me/client/Me.tsx
@@ -1,26 +1,56 @@
-import { IonButton, IonItem, IonLabel } from "@ionic/react";
-import { useQuery } from "@tanstack/react-query";
+import { IonButton, IonInput, IonItem, IonLabel, IonList } from "@ionic/react";
 import { hc } from "hono/client";
+import { useEffect, useState } from "react";
 import { useMe } from "~/hooks/useMe";
 import type { MeAPI } from "~/serverRoutes";
 import { clientUrl } from "~/utils/clientUrl";
-import { fetcher } from "~/utils/fetcher";
 
 export const client = hc<MeAPI>(clientUrl);
 
 export const Me = () => {
-  const { me } = useMe();
+  const { me, refetch } = useMe();
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (me) setName(me?.name ?? "");
+  }, [me]);
 
   return (
-    <IonItem>
-      <IonLabel>{me?.name}</IonLabel>
-      <IonButton
-        onClick={() => {
-          window.open("/auth/login", "_self");
-        }}
-      >
-        google login
-      </IonButton>
-    </IonItem>
+    <IonList>
+      <IonItem>
+        <IonLabel>{me?.name ?? "未ログイン"}</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonButton
+          onClick={() => {
+            window.open("/auth/login", "_self");
+          }}
+        >
+          Google Login
+        </IonButton>
+      </IonItem>
+      <IonItem>
+        <IonInput
+          label="名前"
+          placeholder="名前"
+          value={name}
+          onIonChange={(e) => {
+            setName(e.detail.value ?? "");
+          }}
+        />
+      </IonItem>
+      <IonItem>
+        <IonButton
+          onClick={async () => {
+            await client.api.me.$patch({
+              json: { name },
+            });
+            refetch();
+          }}
+        >
+          保存
+        </IonButton>
+      </IonItem>
+    </IonList>
   );
 };
