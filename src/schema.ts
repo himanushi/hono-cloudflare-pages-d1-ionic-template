a18eaf090d33afc6693495b54d936557e05c1f8a
himanushi <himanushi@users.noreply.github.com>
2024-10-03 20:30:06 +0900

    TODOテーブルの作成とインデックスの追加

--- src/schema.ts
+++ src/schema.ts
@@ -18,3 +18,26 @@ export const users = sqliteTable(
     googleUserIdIndex: index("idx_googleUserId").on(table.googleUserId),
   }),
 );
+
+export const todo = sqliteTable(
+  "todo",
+  {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    title: text("title").notNull(),
+    description: text("description"),
+    status: text("status").default("pending"),
+    userId: integer("user_id")
+      .references(() => users.id)
+      .notNull(),
+    createdAt: integer("created_at", { mode: "timestamp" }).default(
+      sql`(strftime('%s', 'now'))`,
+    ),
+    updatedAt: integer("updated_at", { mode: "timestamp" }).default(
+      sql`(strftime('%s', 'now'))`,
+    ),
+  },
+  (table) => ({
+    statusIndex: index("idx_status").on(table.status),
+    userIdIndex: index("idx_userId").on(table.userId),
+  }),
+);
