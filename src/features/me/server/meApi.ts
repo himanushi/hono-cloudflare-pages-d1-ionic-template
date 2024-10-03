fbff9374b78114820d1498b6ed597cb81a00aaa0
himanushi <himanushi@users.noreply.github.com>
2024-10-03 20:30:36 +0900

    getMeとpatchMeのAPI実装追加

--- src/features/me/server/meApi.ts
+++ src/features/me/server/meApi.ts
@@ -1,25 +1,38 @@
+import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
-import { getSignedCookie } from "hono/cookie";
 import { createFactory } from "hono/factory";
+import { z } from "zod";
 import { users } from "~/schema";
+import { getMe } from "~/utils/getMe";
 
-export const meApi = createFactory().createHandlers(async (c) => {
-  const sub = await getSignedCookie(c, "session", c.env.COOKIE_SECRET);
-  if (typeof sub !== "string") {
-    return c.json(null);
-  }
-
-  const db = drizzle(c.env.DB);
-  const user = await db
-    .select({
-      name: users.name,
-      googleUserId: users.googleUserId,
-    })
-    .from(users)
-    .where(eq(users.googleUserId, sub))
-    .limit(1)
-    .all();
-
-  return c.json(user[0]);
+export const getMeApi = createFactory().createHandlers(async (c) => {
+  const user = await getMe(c);
+  return c.json(user);
 });
+
+export const patchMeApi = createFactory().createHandlers(
+  zValidator(
+    "json",
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  async (c) => {
+    const user = await getMe(c);
+    if (!user) return c.notFound();
+
+    const db = drizzle(c.env.DB);
+    const result = await db
+      .update(users)
+      .set({
+        name: c.req.valid("json").name,
+      })
+      .where(eq(users.id, Number(user.id)))
+      .returning({ updatedUserId: users.id });
+
+    console.log(result);
+
+    return c.text("ok");
+  },
+);
