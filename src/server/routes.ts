288452a246daa75e16e1b0f6327c0e30e97f7629
himanushi <himanushi@users.noreply.github.com>
2024-10-07 20:58:23 +0900

    APIルートの追加と修正

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -18,9 +18,11 @@ const _todoApi = app
   .get("/api/todo", ...getTodoApi)
   .post("/api/todo", ...postTodoApi)
   .patch("/api/todo/:id", ...patchTodoApi);
+
 export type TodoAPI = typeof _todoApi;
 
 const _meApi = app.get("/api/me", ...getMeApi).patch("/api/me", ...patchMeApi);
+
 export type MeAPI = typeof _meApi;
 
 app.get("*", ...server);
