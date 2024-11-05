96993fcc79f2e90e45abe097659bcfb88b5fbd35
himanushi <himanushi@users.noreply.github.com>
2024-11-06 08:41:12 +0900

    テストケースの追加と改良

--- src/server/api/todoApi.test.ts
+++ src/server/api/todoApi.test.ts
@@ -1,36 +1,56 @@
 import { env } from "cloudflare:test";
 import { drizzle } from "drizzle-orm/d1";
-import { afterEach, beforeEach, expect, test, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+import { todo, users } from "~/db/schema";
 import { app } from "~/server/routes";
-import { todo, users } from "../db/schema";
 
 const MOCK_ENV = {
   DB: env.DB,
 };
 
+const user1 = { id: 1, name: "Test User" };
+const user2 = { id: 2, name: "Test User2" };
+
 vi.mock("~/server/utils/getMeMiddleware", () => ({
   getMeMiddleware: (c: any, next: any) => {
-    c.set("me", { id: 1, name: "Test User" });
+    c.set("me", user1);
     return next();
   },
 }));
 
-test("GET /api/todo should return list of todos", async () => {
-  await drizzle(env.DB)
-    .insert(users)
-    .values({ id: 1, name: "Test User", googleUserId: "testGoogleId" })
-    .execute();
+describe("GET /api/todo", async () => {
+  await drizzle(env.DB).insert(users).values(user1).execute();
+  await drizzle(env.DB).insert(users).values(user2).execute();
   await drizzle(env.DB)
     .insert(todo)
-    .values({ title: "Test Todo 1", userId: 1 })
+    .values([
+      { title: "Test Todo 1", userId: user1.id },
+      { title: "Test Todo 2", userId: user1.id },
+    ])
     .execute();
 
-  const res = await app.request("/api/todo?limit=5&offset=0", {}, MOCK_ENV);
-  expect(res.status).toBe(200);
-  const jsonResponse = await res.json();
-  console.log(jsonResponse);
-  expect(jsonResponse).toEqual([
-    { id: 1, title: "Test Todo 1", status: "pending" },
-    { id: 2, title: "Test Todo 2", status: "pending" },
-  ]);
+  it("?limit=5&offset=0", async () => {
+    const res = await app.request("/api/todo?limit=5&offset=0", {}, MOCK_ENV);
+    expect(res.status).toBe(200);
+    const jsonResponse = (await res.json()) as { data: any[] };
+
+    expect(jsonResponse.data).toEqual([
+      { id: 1, title: "Test Todo 1", status: "pending" },
+      { id: 2, title: "Test Todo 2", status: "pending" },
+    ]);
+  });
+
+  it("?limit=1&offset=0&order=desc", async () => {
+    const res = await app.request(
+      "/api/todo?limit=1&offset=0&order=desc",
+      {},
+      MOCK_ENV,
+    );
+    expect(res.status).toBe(200);
+    const jsonResponse = (await res.json()) as { data: any[] };
+
+    expect(jsonResponse.data).toEqual([
+      { id: 2, title: "Test Todo 2", status: "pending" },
+    ]);
+  });
 });
