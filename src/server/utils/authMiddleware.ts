c10b711d0aeea955e7f21d6cf7a7705b021fcb07
himanushi <himanushi@users.noreply.github.com>
2024-11-06 21:45:35 +0900

    型名の変更によるauthMiddlewareの修正

--- src/server/utils/authMiddleware.ts
+++ src/server/utils/authMiddleware.ts
@@ -1,21 +1,23 @@
 import { getSignedCookie } from "hono/cookie";
 import { createMiddleware } from "hono/factory";
-import type { HonoType } from "./createApp";
+import type { HonoPropsType } from "./createApp";
 import { getMe } from "./getMe";
 
-export const authMiddleware = createMiddleware<HonoType>(async (c, next) => {
-  const userId = await getSignedCookie(c, c.env.COOKIE_SECRET, "session");
+export const authMiddleware = createMiddleware<HonoPropsType>(
+  async (c, next) => {
+    const userId = await getSignedCookie(c, c.env.COOKIE_SECRET, "session");
 
-  if (!userId) {
-    return c.json({ error: "Unauthorized" }, 401);
-  }
+    if (!userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
 
-  const me = await getMe(c);
+    const me = await getMe(c);
 
-  if (!me) {
-    return c.json({ error: "User not found" }, 404);
-  }
+    if (!me) {
+      return c.json({ error: "User not found" }, 404);
+    }
 
-  c.set("me", me);
-  await next();
-});
+    c.set("me", me);
+    await next();
+  },
+);
