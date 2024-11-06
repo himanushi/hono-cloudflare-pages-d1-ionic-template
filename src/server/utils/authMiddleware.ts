94a70b78dd61010259ad5768e81d23c80fbf0040
himanushi <himanushi@users.noreply.github.com>
2024-11-06 22:39:31 +0900

    authMiddlewareのエラーメッセージの修正

--- src/server/utils/authMiddleware.ts
+++ src/server/utils/authMiddleware.ts
@@ -5,16 +5,10 @@ import { getMe } from "./getMe";
 
 export const authMiddleware = createMiddleware<HonoPropsType>(
   async (c, next) => {
-    const userId = await getSignedCookie(c, c.env.COOKIE_SECRET, "session");
-
-    if (!userId) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
-
     const me = await getMe(c);
 
     if (!me) {
-      return c.json({ error: "User not found" }, 404);
+      return c.json({ error: "Unauthorized: authMiddleware" }, 401);
     }
 
     c.set("me", me);
