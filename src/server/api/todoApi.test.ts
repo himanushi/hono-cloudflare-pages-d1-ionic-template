e395e782beb726d47b278363c522c7dc335058b4
himanushi <himanushi@users.noreply.github.com>
2024-11-06 21:24:18 +0900

    テスト環境のセットアップと認証処理の改善

--- src/server/api/todoApi.test.ts
+++ src/server/api/todoApi.test.ts
@@ -1,6 +1,6 @@
 import { env } from "cloudflare:test";
 import { drizzle } from "drizzle-orm/d1";
-import { describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { todo, users } from "~/db/schema";
 import { app } from "~/server/routes";
 
@@ -11,23 +11,32 @@ const MOCK_ENV = {
 const user1 = { id: 1, name: "Test User" };
 const user2 = { id: 2, name: "Test User2" };
 
-vi.mock("~/server/utils/authMiddleware", () => ({
-  authMiddleware: (c: any, next: any) => {
-    c.set("me", user1);
-    return next();
-  },
-}));
+describe("GET /api/todo", () => {
+  beforeEach(async () => {
+    // 認証
+    vi.doMock("~/server/utils/authMiddleware", () => ({
+      authMiddleware: (c: any, next: any) => {
+        c.set("me", user1);
+        return next();
+      },
+    }));
 
-describe("GET /api/todo", async () => {
-  await drizzle(env.DB).insert(users).values(user1).execute();
-  await drizzle(env.DB).insert(users).values(user2).execute();
-  await drizzle(env.DB)
-    .insert(todo)
-    .values([
-      { title: "Test Todo 1", userId: user1.id },
-      { title: "Test Todo 2", userId: user1.id },
-    ])
-    .execute();
+    // テストデータ作成
+    await drizzle(env.DB).insert(users).values(user1).execute();
+    await drizzle(env.DB).insert(users).values(user2).execute();
+    await drizzle(env.DB)
+      .insert(todo)
+      .values([
+        { title: "Test Todo 1", userId: user1.id },
+        { title: "Test Todo 2", userId: user1.id },
+        { title: "Test Todo 3", userId: user2.id },
+      ])
+      .execute();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
 
   it("?limit=5&offset=0", async () => {
     const res = await app.request("/api/todo?limit=5&offset=0", {}, MOCK_ENV);
@@ -52,5 +61,36 @@ describe("GET /api/todo", async () => {
     expect(jsonResponse.data).toEqual([
       { id: 2, title: "Test Todo 2", status: "pending" },
     ]);
+  });
+});
+
+describe("Unauthorized GET /api/todo", () => {
+  beforeEach(async () => {
+    vi.doMock("~/server/utils/authMiddleware", () => ({
+      authMiddleware: (c: any, next: any) => {
+        c.set("me", null);
+        return next();
+      },
+    }));
+    // テストデータ作成
+    await drizzle(env.DB).insert(users).values(user1).execute();
+    await drizzle(env.DB).insert(users).values(user2).execute();
+    await drizzle(env.DB)
+      .insert(todo)
+      .values([
+        { title: "Test Todo 1", userId: user1.id },
+        { title: "Test Todo 2", userId: user1.id },
+        { title: "Test Todo 3", userId: user2.id },
+      ])
+      .execute();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("?limit=5&offset=0", async () => {
+    const res = await app.request("/api/todo?limit=5&offset=0", {}, MOCK_ENV);
+    expect(res.status).toBe(401);
   });
 });
