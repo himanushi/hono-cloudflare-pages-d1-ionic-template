50a93d201d3be11989cd42cb573d579590b404b2
himanushi <himanushi@users.noreply.github.com>
2024-11-07 19:52:08 +0900

    ダークテーマのスタイルを追加

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -8,6 +8,7 @@ import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
+import "@ionic/react/css/palettes/dark.always.css";
 
 import { IonApp, setupIonicReact } from "@ionic/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
